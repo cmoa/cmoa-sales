82ef06ce95a40110638021f3f2637598a01d8a91
Patrick Burke <pat.burke@carney.co>
2018-07-27 15:10:34 -0400

    Removes extraneous <h2> and adds label classes with JS.

--- assets/scripts/ecomm.js
+++ assets/scripts/ecomm.js
@@ -1,4 +1,6 @@
 export function addPlaceholders() {
+  $('.member-login, .apply-special').find('label').addClass('level-2');
+  $('.member-login > h2').remove();
   $('#special-code').attr('placeholder', 'Enter code');
   $('#member_number').attr('placeholder', 'Member number');
 }
